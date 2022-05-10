c4568218e9159a070738386ba447ae01c5cc6239
Max Teclav <maks.tetslav@yandex.ru>
2022-05-10 23:07:19 +0300

    V-08 feat: implement orderCheckList component

---
+++ src/components/common/orderCheckList/OrderCheckList.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import getOrderOptions from '@utils/helpers/getOrderOptionsData';
+import Checkbox from '../checkbox/Checkbox';
+import cl from './OrderCheckList.module.scss';
+
+interface IOrderCheckListProps {
+  tank: boolean;
+  chair: boolean;
+  wheel: boolean;
+}
+
+const OrderCheckList: React.FC<IOrderCheckListProps> = ({
+  tank,
+  chair,
+  wheel,
+}) => {
+  return (
+    <div className={cl.container}>
+      {getOrderOptions(tank, chair, wheel).map((item) => (
+        <Checkbox
+          title={item.name}
+          id={`${item.type}${item.id}`}
+          key={item.id}
+          status={item.status}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default OrderCheckList;
