e64ec6f59d131d987bf7a48c12a9d3319939bfa0
Max Teclav <maks.tetslav@yandex.ru>
2022-05-16 01:02:59 +0300

    V-09/F-10 feat: add constants for tables

---
+++ src/utils/constants/tables.ts
@@ -0,0 +1,67 @@
+import { ITableHeader } from '@models/app';
+
+export const DATA_ERROR_MESSAGE = 'Нет данных';
+
+export const CATEGORY_HEADERS: ITableHeader[] = [
+  {
+    name: 'Название',
+    key: 'name',
+  },
+  {
+    name: 'Описание',
+    key: 'description',
+  },
+  {
+    name: 'ID',
+    key: 'id',
+  },
+];
+
+export const NAME_AND_ID_HEADERS: ITableHeader[] = [
+  {
+    name: 'Название',
+    key: 'name',
+  },
+  {
+    name: 'ID',
+    key: 'id',
+  },
+];
+
+export const POINT_HEADERS: ITableHeader[] = [
+  {
+    name: 'Название',
+    key: 'name',
+  },
+  {
+    name: 'Город',
+    key: 'city',
+  },
+  {
+    name: 'Адресс',
+    key: 'address',
+  },
+  {
+    name: 'ID',
+    key: 'id',
+  },
+];
+
+export const RATE_HEADERS: ITableHeader[] = [
+  {
+    name: 'Название',
+    key: 'name',
+  },
+  {
+    name: 'Длительность',
+    key: 'duration',
+  },
+  {
+    name: 'Цена',
+    key: 'price',
+  },
+  {
+    name: 'ID',
+    key: 'id',
+  },
+];
