40133f7214cdbd4cec96c866c53e610c370acbe6
Max Teclav <maks.tetslav@yandex.ru>
2022-05-27 06:32:36 +0300

    V-10 feat: improve animation

--- src/components/common/userInfoButton/UserInfoButton.tsx
+++ src/components/common/userInfoButton/UserInfoButton.tsx
@@ -1,4 +1,5 @@
 import React, { useCallback, useState } from 'react';
+import classNames from 'classnames';
 import avatarIcon from '@assets/svg/avatar.svg';
 import dropdownIcon from '@assets/svg/dropdown.svg';
 import UserInfoDropdown from '../userInfoDropdown/UserInfoDropdown';
@@ -11,16 +12,24 @@ const UserInfoButton: React.FC = () => {
     setIsOpen((state) => !state);
   }, []);
 
+  const buttonClasses = classNames(cl.button, { [cl.animation]: isOpen });
+
   return (
     <div className={cl.container}>
-      <button className={cl.button} onClick={clickHandler} type="button">
-        <img className={cl.avatar} src={avatarIcon} alt="user" />
-        <p className={cl.username}>
-          Admin
-          <img className={cl.dropdownIcon} src={dropdownIcon} alt="dropdown" />
-        </p>
-      </button>
       <UserInfoDropdown isOpen={isOpen} />
+      <button className={buttonClasses} onClick={clickHandler} type="button">
+        <span>
+          <img className={cl.avatar} src={avatarIcon} alt="user" />
+          <p className={cl.username}>
+            Admin
+            <img
+              className={cl.dropdownIcon}
+              src={dropdownIcon}
+              alt="dropdown"
+            />
+          </p>
+        </span>
+      </button>
     </div>
   );
 };
