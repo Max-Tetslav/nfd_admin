fecb78d8997246392b3489dc0366a3fd4e1aac16
Max Teclav <maks.tetslav@yandex.ru>
2022-05-27 06:56:54 +0300

    V-10/F-09 feat: add action handlers

--- src/components/common/orderCard/OrderCard.tsx
+++ src/components/common/orderCard/OrderCard.tsx
@@ -1,18 +1,27 @@
-import React, { useMemo } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import classNames from 'classnames';
 import { Button } from 'antd';
-import { IOrderData } from '@models/data';
+import { IOrderData, TPostOrderData } from '@models/data';
 import completeIcon from '@assets/svg/complete.svg';
 import cancelIcon from '@assets/svg/cancel.svg';
 import editIcon from '@assets/svg/edit.svg';
+import noPhotoIcon from '@assets/svg/no-photo.svg';
 import formatPrice from '@utils/helpers/formatPrice';
 import formatDate from '@utils/helpers/formatDate';
 import { NO_PHOTO } from '@utils/constants/tables';
+import useModalConfirm from '@hooks/useModalConfirm/useModalConfirm';
+import {
+  updateOrderStatusCancel,
+  updateOrderStatusComplete,
+} from '@store/reducers/form';
+import { useAppDispatch } from '@store/store';
 import OrderCheckList from '../orderCheckList/OrderCheckList';
 import cl from './OrderCard.module.scss';
 
 interface IOrderCardProps {
   order: IOrderData;
+  actionHandler: (id: string, orderData: TPostOrderData) => Promise<unknown>;
+  editHandler: (id: string) => void;
 }
 
 const OrderCard: React.FC<IOrderCardProps> = ({
@@ -27,8 +36,89 @@ const OrderCard: React.FC<IOrderCardProps> = ({
     isFullTank,
     isNeedChildChair,
     isRightWheel,
+    rateId,
+    id,
   },
+  actionHandler,
+  editHandler,
 }) => {
+  const dispatch = useAppDispatch();
+  const completeConfirm = useModalConfirm('complete');
+  const cancelConfirm = useModalConfirm('cancel');
+
+  const getUpdatedOrder = useCallback((statusId: string) => {
+    return {
+      price: price || 0,
+      carId: {
+        id: carId?.id || '',
+      },
+      cityId: {
+        id: cityId?.id || '',
+      },
+      color: color || '',
+      isFullTank,
+      isNeedChildChair,
+      isRightWheel,
+      pointId: {
+        id: pointId?.id || '',
+      },
+      orderStatusId: {
+        id: statusId,
+      },
+      dateFrom: dateFrom || 0,
+      dateTo: dateTo || 0,
+      rateId: {
+        id: rateId?.id || '',
+      },
+    };
+  }, []);
+
+  const completeHandler = useCallback(() => {
+    actionHandler(id, getUpdatedOrder('6278c4ba9535e90010bfaf36')).then(
+      (data) => {
+        const result = (
+          data as {
+            data: unknown;
+          }
+        ).data;
+
+        dispatch(updateOrderStatusComplete(Boolean(result)));
+        setTimeout(() => {
+          dispatch(updateOrderStatusComplete(null));
+        }, 4000);
+      },
+    );
+  }, []);
+
+  const cancelHandler = useCallback(() => {
+    actionHandler(id, getUpdatedOrder('5e26a1f5099b810b946c5d8c')).then(
+      (data) => {
+        const result = (
+          data as {
+            data: unknown;
+          }
+        ).data;
+
+        dispatch(updateOrderStatusCancel(Boolean(result)));
+        setTimeout(() => {
+          dispatch(updateOrderStatusCancel(null));
+        }, 4000);
+      },
+    );
+  }, []);
+
+  const onCancel = useCallback(() => {
+    cancelConfirm(null, cancelHandler);
+  }, []);
+
+  const onComplete = useCallback(() => {
+    completeConfirm(null, completeHandler);
+  }, []);
+
+  const onEdit = useCallback(() => {
+    editHandler(id);
+  }, []);
+
   const address = useMemo(() => {
     if (!cityId?.name && !pointId?.address && !pointId?.name) {
       return ' Адрес не указан';
@@ -47,7 +137,7 @@ const OrderCard: React.FC<IOrderCardProps> = ({
     <div className={cl.container}>
       <img
         className={cl.img}
-        src={carId?.thumbnail.path || ''}
+        src={carId?.thumbnail.path || noPhotoIcon}
         alt={carId?.name || NO_PHOTO}
       />
       <div className={cl.infoBox}>
@@ -72,7 +162,7 @@ const OrderCard: React.FC<IOrderCardProps> = ({
       <div className={cl.buttonBox}>
         <Button
           className={classNames(cl.button, cl.completeButton)}
-          // onClick={completeHandler}
+          onClick={onComplete}
           icon={
             <img className={cl.buttonIcon} src={completeIcon} alt="complete" />
           }
@@ -81,14 +171,14 @@ const OrderCard: React.FC<IOrderCardProps> = ({
         </Button>
         <Button
           className={classNames(cl.button, cl.cancelButton)}
-          // onClick={cancelHandler}
+          onClick={onCancel}
           icon={<img className={cl.buttonIcon} src={cancelIcon} alt="cancel" />}
         >
           Отмена
         </Button>
         <Button
           className={classNames(cl.button, cl.editButton)}
-          // onClick={editHandler}
+          onClick={onEdit}
           icon={<img className={cl.buttonIcon} src={editIcon} alt="edit" />}
         >
           Изменить
