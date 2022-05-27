88b946c90b49d6644e245d25198ed236767ab090
Max Teclav <maks.tetslav@yandex.ru>
2022-05-27 07:02:59 +0300

    V-10/F-09 feat: improve menu

--- src/components/containers/menuList/MenuList.tsx
+++ src/components/containers/menuList/MenuList.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import { Switch } from 'antd';
 import MenuItem from '@components/common/menuItem/MenuItem';
 import menuData from '@utils/constants/menuData';
 import cl from './MenuList.module.scss';
 
 const MenuList: React.FC = () => {
+  const [addMode, setAddMode] = useState(false);
+
+  const switchHandler = useCallback(() => {
+    setAddMode((state) => !state);
+  }, []);
+
   return (
-    <ul className={cl.container}>
-      {menuData.map((item) => {
-        return <MenuItem data={item} key={item.id} />;
-      })}
-    </ul>
+    <>
+      <ul className={cl.container}>
+        {menuData.map((item) => {
+          return <MenuItem data={item} key={item.id} isAddMode={addMode} />;
+        })}
+      </ul>
+      <div className={cl.switchBox}>
+        <span className={cl.text}>Добавить сущности:</span>
+        <Switch onChange={switchHandler} />
+      </div>
+    </>
   );
 };
 
