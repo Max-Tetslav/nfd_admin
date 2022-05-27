d819248f49788fc20dfffabd94bf71751e810999
Max Teclav <maks.tetslav@yandex.ru>
2022-05-27 06:45:20 +0300

    V-10/F-09 feat: implement editCheckbox component

---
+++ src/components/common/editCheckbox/EditCheckbox.tsx
@@ -0,0 +1,34 @@
+import React, { ChangeEvent, useCallback } from 'react';
+import { useField, useFormikContext } from 'formik';
+import { ICarFormValues } from '@models/app';
+import Checkbox from '../checkbox/Checkbox';
+
+interface IFilterSelectProps {
+  label: string;
+  type: string;
+  name: string;
+  adminEdit?: boolean;
+}
+
+const EditCheckbox: React.FC<IFilterSelectProps> = ({ label, ...props }) => {
+  const [field, , helpers] = useField(props);
+  const { setFieldValue } = useFormikContext<ICarFormValues>();
+
+  const changeHandler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setFieldValue(field.name, e.target.checked);
+    helpers.setTouched(true);
+  }, []);
+
+  return (
+    <Checkbox
+      title={label}
+      id={field.name}
+      status={field.value}
+      readOnly={false}
+      changeHandler={changeHandler}
+      adminEdit={props.adminEdit}
+    />
+  );
+};
+
+export default EditCheckbox;
