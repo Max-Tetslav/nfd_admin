0120e3eca0523f1ed6785238178b181e21684267
Max Teclav <maks.tetslav@yandex.ru>
2022-05-27 07:15:53 +0300

    V-10/F-09 feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,23 @@ import TableCategory from '@components/containers/tableCategory/TableCategory';
 import TableRates from '@components/containers/tableRates/TableRates';
 import TablePoints from '@components/containers/tablePoints/TablePoints';
 import TableOrderStatus from '@components/containers/tableOrderStatus/TableOrderStatus';
+import AdminError from '@components/common/adminError/AdminError';
+import AddCategory from '@components/containers/tableCategory/addCategory/AddCategory';
+import AddCity from '@components/containers/tableCity/addCity/AddCity';
+import AddPoint from '@components/containers/tablePoints/addPoint/AddPoint';
+import AddRate from '@components/containers/tableRates/addRate/AddRate';
+import AddOrderStatus from '@components/containers/tableOrderStatus/addOrderStatus/AddOrderStatus';
+import EditCity from '@components/containers/tableCity/editCity/EditCity';
+import EditOrderStatus from '@components/containers/tableOrderStatus/editOrderStatus/EditOrderStatus';
+import EditRate from '@components/containers/tableRates/editRate /EditRate';
+import EditPoint from '@components/containers/tablePoints/editPoint/EditPoint';
+import EditCategory from '@components/containers/tableCategory/editCategory/EditCategory';
+import AddCar from '@components/containers/tableCars/addCar/AddCar';
+import EditCar from '@components/containers/tableCars/editCar/EditCar';
+import AddRateType from '@components/containers/tableRateType/addRateType/AddRateType';
+import TableRateType from '@components/containers/tableRateType/TableRateType';
+import EditRateType from '@components/containers/tableRateType/editRateType/EditRateType';
+import EditOrder from '@components/containers/tableOrders/editOrder/EditOrder';
 
 const App: React.FC = () => {
   return (
@@ -27,13 +44,46 @@ const App: React.FC = () => {
               </RequireAuth>
             }
           >
-            <Route path="order" element={<TableOrders />} />
-            <Route path="car" element={<TableCars />} />
-            <Route path="category" element={<TableCategory />} />
-            <Route path="city" element={<TableCity />} />
-            <Route path="point" element={<TablePoints />} />
-            <Route path="rate" element={<TableRates />} />
-            <Route path="status" element={<TableOrderStatus />} />
+            <Route path="order">
+              <Route index element={<TableOrders />} />
+              <Route path=":id" element={<EditOrder />} />
+            </Route>
+            <Route path="car">
+              <Route index element={<TableCars />} />
+              <Route path="add" element={<AddCar />} />
+              <Route path=":id" element={<EditCar />} />
+            </Route>
+            <Route path="category">
+              <Route index element={<TableCategory />} />
+              <Route path="add" element={<AddCategory />} />
+              <Route path=":id" element={<EditCategory />} />
+            </Route>
+            <Route path="city">
+              <Route index element={<TableCity />} />
+              <Route path="add" element={<AddCity />} />
+              <Route path=":id" element={<EditCity />} />
+            </Route>
+            <Route path="point">
+              <Route index element={<TablePoints />} />
+              <Route path="add" element={<AddPoint />} />
+              <Route path=":id" element={<EditPoint />} />
+            </Route>
+            <Route path="rate">
+              <Route index element={<TableRates />} />
+              <Route path="add" element={<AddRate />} />
+              <Route path=":id" element={<EditRate />} />
+            </Route>
+            <Route path="tariffType">
+              <Route index element={<TableRateType />} />
+              <Route path="add" element={<AddRateType />} />
+              <Route path=":id" element={<EditRateType />} />
+            </Route>
+            <Route path="status">
+              <Route index element={<TableOrderStatus />} />
+              <Route path="add" element={<AddOrderStatus />} />
+              <Route path=":id" element={<EditOrderStatus />} />
+            </Route>
+            <Route path="*" element={<AdminError code={404} />} />
           </Route>
         </Routes>
       </Provider>
